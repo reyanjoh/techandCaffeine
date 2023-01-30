0721999487eb478dae20814e2d943d64b4158327
reyan <2001103609@student.buksu.edu.ph>
2023-01-30 08:48:23 +0800

    minor fix

--- src/components/universalComponents/ServiceCard.js
+++ src/components/universalComponents/ServiceCard.js
@@ -3,9 +3,9 @@ import React from 'react'
 const ServiceCard = ({title}) => {
   return (
     <div className='ServiceCard'>
-        <a className='font-color-black' href=''>
+        <a className='font-color-black' href='/services'>
             <div className='img-frame'>
-                <img alt='service-img' className='ServiceCard-img' style={{
+                <img alt='' className='ServiceCard-img' style={{
                     width: '100%',
                     transition: 'transform .5s'
                 }} src='./assets/imgs/services/hard-disk.jpg' />
@@ -16,7 +16,7 @@ const ServiceCard = ({title}) => {
 
                 <span className='chage-color-on-hover' style={{margin: '1rem 0'}}>Maecenas lacinia neque quis lacus ullamcorper posuere. Pellentesque at dui a purus elementum scelerisque a quis ante.</span>
 
-                <a className='chage-color-on-hover black' href='#'>Learn More. . .</a>
+                <a className='chage-color-on-hover black' href='/services'>Learn More. . .</a>
             </div>
         </a>  
     </div>
